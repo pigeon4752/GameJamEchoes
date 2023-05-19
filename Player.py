b7a1d57953690b5275be80926b10e613e90e6759
pigeon4752 <b.antrobus@lancaster.ac.uk>
2023-05-19 16:48:03 +0100

    added gravity and delta time movement

--- Player.py
+++ Player.py
@@ -4,10 +4,10 @@ class Player(Sprite):
     moveSpeed = 5
     gravity = 1
 
-    def update(self):
-        self.changeYVelocity(self.gravity)
+    def update(self,dt):
+        self.changeYVelocity(self.gravity*dt)
         # self.updatePosition()
-        super().update()
+        super().update(dt)
 
     def __init__(self,screen,background,moveSpeed):
         super().__init__(screen,background)
