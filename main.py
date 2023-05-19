bb42fc3ca9e4a0b11bee765902923f944948347f
pigeon4752 <b.antrobus@lancaster.ac.uk>
2023-05-19 12:10:32 +0100

    added key handling

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
+
+
+
 class main:
+
+    def handleKeys(keys):
+        if keys[pygame.K_a]:
+            print("'A' key is being pressed")
+
+
     pygame.init()
     screen = pygame.display.set_mode([500, 500])
+    clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
-
                 running = False
 
+        # Get state of all keys
+        keys = pygame.key.get_pressed()
+        handleKeys(keys)
+        
         screen.fill((255, 255, 255))
 
 
@@ -21,6 +33,7 @@ class main:
         # Flip the display
 
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 # Done! Time to quit.
