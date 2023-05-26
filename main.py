6409c07df64d311ac542a82aca77c6801d5764f7
pigeon4752 <b.antrobus@lancaster.ac.uk>
2023-05-26 16:50:15 +0100

    flipped story to true

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class main:
         pygame.mixer.music.set_endevent(pygame.USEREVENT+1)
         pygame.mixer.music.play()
 
-    story = False # SET TO FALSE FOR NO STORY
+    story = True # SET TO FALSE FOR NO STORY
     playMusic("IntroSong.mp3")
     pygame.mixer.Sound("munch.mp3").play()
     storyImage = pygame.image.load(os.path.join("story0.png"))
